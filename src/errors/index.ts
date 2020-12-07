895b153a560cd4babd511828d0d7c2951cc686f0
Sarcasmonaut <patrik.kessler@outlook.de>
2020-12-07 20:43:00 +0100

    add 403 & 404

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,8 +1,19 @@
 export class InternalServerError extends Error {
-  public statusCode = 500
+  public name = "InternalServerError";
+  public statusCode = 500;
 }
 
 export class BadRequestError extends Error {
-  public name = "BadRequest"
-  public statusCode = 400
+  public name = "BadRequest";
+  public statusCode = 400;
+}
+
+export class ForbiddenError extends Error {
+  public name = "ForbiddenError";
+  public statusCode = 403;
+}
+
+export class NotFoundError extends Error {
+  public name = "NotFoundError";
+  public statusCode = 404;
 }
